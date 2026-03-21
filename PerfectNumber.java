b84ab344de439b6601b37f03104627aaf7820bfb
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-21 16:12:46 +0530

    Create PerfectNumber.java

---
+++ PerfectNumber.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+
+        if(n==1){
+            System.out.print("NO");
+            return;
+        }
+        int sum=1;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                sum+=i;
+                if(i!=n/i){
+                    sum+= n/i;
+                }
+            }
+        }
+        if(sum== n){
+            System.out.print("YES");
+
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
