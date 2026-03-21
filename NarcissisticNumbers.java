101ce0c8531aeba528f61472474e3210d5146f71
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-21 16:15:39 +0530

    Create NarcissisticNumbers.java

---
+++ NarcissisticNumbers.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=n,sum=0;
+
+        int digits= (n==0)?1:(int)Math.log10(n)+1;
+
+        while(n>0){
+            int d=n%10;
+            sum+=(int)Math.pow(d,digits);
+            n/=10;
+        }
+        if(sum== temp)
+        System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+    
+}
