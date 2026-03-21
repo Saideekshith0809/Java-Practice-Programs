5fc4a65767b75eceaa100c6e2e589560447d1bef
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-21 16:18:06 +0530

    Create Gauntlets.java

---
+++ Gauntlets.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt(),pairs=0;
+        int [] f=new int[1001];
+
+        for(int i=0;i<n;i++) f[sc.nextInt()]++;
+        for(int i=0;i<1000;i++) pairs+=f[i]/2;
+         System.out.print(pairs);
+
+    }
+}
