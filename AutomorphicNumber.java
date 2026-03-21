97b0b40db294cfc667bf8909c0c4640a1b6c4e5d
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-21 16:16:33 +0530

    Create AutomorphicNumber.java

---
+++ AutomorphicNumber.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int sq=n*n;
+
+        int digits= (n==0)?1:(int)Math.log10(n)+1;
+
+        int  mod=(int)Math.pow(10,digits);
+
+        if(sq%mod==n)
+        System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+}
