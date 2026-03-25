783fe4a9b3eb2f66160744154dee05ca7e8b8ea3
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-25 22:00:11 +0530

    Create MatrixRowSum.java

---
+++ MatrixRowSum.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i,j,m,n;
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        m=sc.nextInt();
+        n=sc.nextInt();
+        for(i=0;i<m;i++){
+            sum=0;
+        
+        for(j=0;j<n;j++){
+            sum+=sc.nextInt();
+        }
+        System.out.println(sum);
+        }
+    }
+}
