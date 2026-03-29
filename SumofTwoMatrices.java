b2cd853e71c561675c8b1760e2dbb3880859a5d6
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-29 23:17:54 +0530

    Add SumofTwoMatrices.java for matrix addition

---
+++ SumofTwoMatrices.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int m,n,i,j;
+        Scanner sc= new Scanner(System.in);
+        m=sc.nextInt();
+        n=sc.nextInt();
+        int [][]a= new int[m][n];
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                a[i][j]+=sc.nextInt();
+            }
+        }
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
+        
+
+    }
+}
