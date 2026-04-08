d9564d75204708ae8e45c799e9186da7d6ec1139
Sai deekshith <orugantideekshith2006@gmail.com>
2026-04-08 21:38:50 +0530

    Add Movie.java with cost calculation logic

---
+++ Movie.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    
+	    while(t-- > 0){
+	        int N = sc.nextInt();
+	        int M = sc.nextInt();
+	        int A = sc.nextInt();
+	        int B = sc.nextInt();
+	        int C = sc.nextInt();
+	        
+	        int combos = Math.min(N, M);
+	        
+	        int cost = combos * C 
+	                 + (N - combos) * A 
+	                 + (M - combos) * B;
+	        
+	        System.out.println(cost);
+	    }
+	}
+}
