e3ebb528ceb180264c6da268042693d6ffd7a4e3
Sai deekshith <orugantideekshith2006@gmail.com>
2026-04-08 21:39:55 +0530

    Add CakeDiscount.java for calculating cake costs

---
+++ CakeDiscount.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    
+	    int cost;
+	    
+	    if(n >= 5){
+	        cost = n * 85;
+	    } else {
+	        cost = n * 100;
+	    }
+	    
+	    System.out.println(cost);
+	}
+}
