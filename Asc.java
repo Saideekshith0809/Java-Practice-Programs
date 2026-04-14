40a1e9308b102de56b9e048cc53379373421b6d7
Sai deekshith <orugantideekshith2006@gmail.com>
2026-04-14 22:56:23 +0530

    Refactor name sorting to use Arrays.sort
    Updated the name sorting logic to use Arrays.sort for simplicity and improved input handling for names with spaces.

--- Asc.java
+++ Asc.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
-class Asc
-{
-public static void main(String args[])
-{
-Scanner sc=new Scanner(System.in);
-int n;
-System.out.println("Enter the no.of names");
-n=sc.nextInt();
-String str[]=new String[n];
-System.out.println("Enter the names ");
-for(int i=0;i<n;i++)
-{
-str[i]=sc.next();
+import java.util.Arrays;
+
+class Asc {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of names:");
+        int n = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        String[] str = new String[n];
+
+        System.out.println("Enter the names:");
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.nextLine(); // allows full names with spaces
+        }
+
+        // Sorting in ascending order
+        Arrays.sort(str);
+
+        System.out.println("The sorted names are:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(str[i]);
+        }
+
+        sc.close();
+    }
 }
-String temp;
-for(int i=0; i<n; i++)
-{
-for(int j=i+1; j<n; j++)
-{
-if((str[i].compareTo(str[j]))>0)
-{
-temp=str[i];
-str[i]=str[j];
-str[j]=temp;
-}
-}
-}
-System.out.println("The sorted names are :");
-for(int i=0; i<n;i++)
-System.out.print(str[i]+ "  ");
-}}
