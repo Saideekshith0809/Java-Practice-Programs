ec71fb6374d000c555b875fc34d238af2e81c395
Sai deekshith <orugantideekshith2006@gmail.com>
2026-06-11 21:48:42 +0530

    Create RomanToInteger.java

---
+++ RomanToInteger.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int romanToInt(String s) {
+        int ans=0;
+        for(int i=0;i<s.length()-1;i++){
+            if(value(s.charAt(i))<value(s.charAt(i+1))){
+                ans-=value(s.charAt(i));
+            }
+            else{
+                ans+=value(s.charAt(i));
+            }
+           
+        } 
+        ans += value(s.charAt(s.length() - 1));
+        return ans;
+
+        
+    }
+    int value(char c) {
+        if (c == 'I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        return 1000; 
+    }
+}
