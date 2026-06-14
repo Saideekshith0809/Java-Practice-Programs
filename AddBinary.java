5f8138ba6109c92f1a46a59c8e7452121231a6ce
Sai deekshith <orugantideekshith2006@gmail.com>
2026-06-14 22:45:40 +0530

    Update AddBinary.java

--- AddBinary.java
+++ AddBinary.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.Scanner;
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder result = new StringBuilder();
