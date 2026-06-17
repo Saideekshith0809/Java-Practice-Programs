e14bdedf9dc97a787437305025a38758db9d1f69
Sai deekshith <orugantideekshith2006@gmail.com>
2026-06-17 21:58:09 +0530

    Update LengthOfLastWord.java

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -6,12 +6,9 @@ class Solution {
         int len = 0;
         int i = s.length() - 1;
 
-        // Skip trailing spaces
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
-
-        // Count characters of last word
         while (i >= 0 && s.charAt(i) != ' ') {
             len++;
             i--;
