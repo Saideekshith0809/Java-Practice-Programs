323accea3fcf477d537f01978e6eb7f26d8bae48
Sai deekshith <orugantideekshith2006@gmail.com>
2026-06-27 13:11:04 +0530

    Add Scanner import to permutations.java

--- permutations.java
+++ permutations.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.Scanner;
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
