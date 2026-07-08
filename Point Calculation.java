78633ab0e68f2fde2526200a79ae0a9a29a65265
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-08 20:24:53 +0530

    Implement point calculation in Codechef class

--- Point Calculation.java
+++ Point Calculation.java
@@ -1,1 +1,16 @@
-
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int a[]=  new int[2];
+	    Scanner s = new Scanner(System.in);
+	    for(int i=0;i<2;i++){
+	        a[i]=s.nextInt();
+	    }
+	   int A=a[0]*3;
+	    int B=a[1]*1;
+	    
+	    int total=A+B;
+	    System.out.println(total);
+	}
+}
