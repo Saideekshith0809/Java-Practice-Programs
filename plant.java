4d1400b46e2a58bd9c12a2f8173c318b872e9b4c
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-08 21:27:50 +0530

    Update plant.java

--- plant.java
+++ plant.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+            }
+            
+            int maxHeight = 0;
+            // Try planting on each possible day (0-indexed: 0 to N-2)
+            for (int i = 0; i < N - 1; i++) {
+                int height = Math.min(A[i], A[i + 1]);
+                maxHeight = Math.max(maxHeight, height);
+            }
+            
+            sb.append(maxHeight).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
