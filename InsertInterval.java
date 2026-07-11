c614e884846f55a5e297b71c93d05b9239a18e6d
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-11 23:25:58 +0530

    Update InsertInterval.java

--- InsertInterval.java
+++ InsertInterval.java
@@ -9,7 +9,6 @@ class Solution {
             i++;
         }
 
-        // Merge overlapping intervals
         while (i < n && intervals[i][0] <= newInterval[1]) {
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
