333964459a081e6446746fc90805edd56d757191
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-15 20:50:53 +0530

    Update Substring Shift.java

--- Substring Shift.java
+++ Substring Shift.java
@@ -1,1 +1,27 @@
+import java.util.*;
 
+class Codechef {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            char[] s = sc.next().toCharArray();
+
+            int i = 0;
+
+            while (i < n && s[i] != 'z') {
+                i++;
+            }
+
+            while (i < n && s[i] == 'z') {
+                s[i] = 'a';
+                i++;
+            }
+
+            System.out.println(s);
+        }
+    }
+}
