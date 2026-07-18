feec46ae4c31074987f2eadb2a1de64f54d65346
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-18 21:26:33 +0530

    Update Rearrange String to Avoid Character Pair.java

--- Rearrange String to Avoid Character Pair.java
+++ Rearrange String to Avoid Character Pair.java
@@ -2,23 +2,17 @@ class Solution {
     public String rearrangeString(String s, char x, char y) {
         char[] ans = new char[s.length()];
         int k = 0;
-
-        // Characters other than x and y
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (ch != x && ch != y) {
                 ans[k++] = ch;
             }
         }
-
-        // All y's
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == y) {
                 ans[k++] = y;
             }
         }
-
-        // All x's
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == x) {
                 ans[k++] = x;
