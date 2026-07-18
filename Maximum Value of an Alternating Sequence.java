158c0c2637133845e7b25c2d188e9a0031548f21
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-18 21:27:47 +0530

    Optimize maximum value calculation logic
    Refactor logic for calculating maximum value in alternating sequence.

--- Maximum Value of an Alternating Sequence.java
+++ Maximum Value of an Alternating Sequence.java
@@ -5,11 +5,9 @@ class Solution {
 
         long ans = s;
 
-        // Case 1: s is a peak
         long peaks1 = (n - 1) / 2;
         ans = Math.max(ans, s + peaks1 * (m - 1L));
 
-        // Case 2: s is a valley
         if (n >= 2) {
             long peaks2 = n / 2;
             ans = Math.max(ans, s + peaks2 * (m - 1L) + 1);
