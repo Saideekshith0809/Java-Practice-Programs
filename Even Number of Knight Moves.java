5ac11808458f1c6f6c94508fa979de470fb36959
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-19 22:25:29 +0530

    Update Even Number of Knight Moves.java

--- Even Number of Knight Moves.java
+++ Even Number of Knight Moves.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.util.Scanner;
 class Solution {
     public boolean canReach(int[] start, int[] target) {
         return (start[0] + start[1]) % 2 == (target[0] + target[1]) % 2;
