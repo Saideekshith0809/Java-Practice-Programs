1d3dc10be6d16594d9d00b58afed8e9640cc0624
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-21 21:38:57 +0530

    Add bit flipping solution in Bit Flipping.java
    Implement a solution to count the number of bit flips needed to convert a number to its binary representation.

--- Bit Flipping.java
+++ Bit Flipping.java
@@ -1,1 +1,16 @@
+public class Solution {
+    public int solve(int A) {
+        int ans = 0;
+        int place = 1;
 
+        while (A > 0) {
+            if ((A & 1) == 0) {
+                ans += place;
+            }
+            place <<= 1;
+            A >>= 1;
+        }
+
+        return ans;
+    }
+}
