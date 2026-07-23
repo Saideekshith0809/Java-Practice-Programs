71d5d243cc20abd9ab61bfa784421af2370708da
Sai deekshith <orugantideekshith2006@gmail.com>
2026-07-23 23:02:59 +0530

    Update PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.lang.*;
 import java.util.*;
+import java.util.Scanner;
 class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
